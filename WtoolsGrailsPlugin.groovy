e55b6e8b4645b16cd32c756dc81440a81918d1aa
click012 <jaeuk012@hanmail.net>
2013-07-17 17:53:36 +0900

    edit plugin descriptor
    http://grails.org/plugins/pending/142#comment-965404147

--- WtoolsGrailsPlugin.groovy
+++ WtoolsGrailsPlugin.groovy
@@ -1,18 +1,18 @@
 class WtoolsGrailsPlugin {
-    // the plugin version
-    def version = "0.1"
-    // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "2.2 > *"
-    // resources that are excluded from plugin packaging
-    def pluginExcludes = [
-        "grails-app/views/error.gsp"
-    ]
+	// the plugin version
+	def version = "0.1"
+	// the version or versions of Grails the plugin is designed for
+	def grailsVersion = "2.2 > *"
+	// resources that are excluded from plugin packaging
+	def pluginExcludes = [
+		"grails-app/views/error.gsp"
+	]
 
-    // TODO Fill in these fields
-    def title = "Wtools Plugin" // Headline display name of the plugin
-    def author = "JaeukJung"
-    def authorEmail = "jaeuk012@hanmail.net"
-    def description = '''\
+
+	def title = "Wtools Plugin" // Headline display name of the plugin
+	def author = "Jaeuk Jung"
+	def authorEmail = "jaeuk012@hanmail.net"
+	def description = '''\
 Ask to be excused. I have poor english...T.T, 
 You check correct wrong text...plz... 
 Thank you.
@@ -36,54 +36,49 @@ Attribute 'timeout' is assured gsp page compile time.
 	500 msec
 '''
 
-    // URL to the plugin's documentation
-    def documentation = "http://grails.org/plugin/wtools"
+	// URL to the plugin's documentation
+	def documentation = "http://grails.org/plugin/wtools"
 
-    // Extra (optional) plugin metadata
+	// Extra (optional) plugin metadata
 
-    // License: one of 'APACHE', 'GPL2', 'GPL3'
-//    def license = "APACHE"
+	// License: one of 'APACHE', 'GPL2', 'GPL3'
+	def license = "APACHE"
 
-    // Details of company behind the plugin (if there is one)
-//    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
+	// Details of company behind the plugin (if there is one)
+	// def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
 
-    // Any additional developers beyond the author specified above.
-//    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
+	// Any additional developers beyond the author specified above.
+	def developers = [ [ name: "Jaeuk Jung", email: "jaeuk012@hanmail.net" ]]
 
-    // Location of the plugin's issue tracker.
-//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
+	// Location of the plugin's issue tracker.
+	// def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
+	def issueManagement = [ system: "GIT", url: "https://github.com/click012/wtools/issues" ]
 
-    // Online location of the plugin's browseable source code.
-    def scm = [ url: "https://github.com/click012/wtools" ]
 
-    def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional), this event occurs before
-    }
+	// Online location of the plugin's browseable source code.
+	def scm = [ url: "https://github.com/click012/wtools" ]
 
-    def doWithSpring = {
-        // TODO Implement runtime spring config (optional)
-    }
 
-    def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
-    }
+	/*
+	def doWithWebDescriptor = { xml ->
+	}
 
-    def doWithApplicationContext = { applicationContext ->
-        // TODO Implement post initialization spring config (optional)
-    }
+	def doWithSpring = {
+	}
 
-    def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
+	def doWithDynamicMethods = { ctx ->
+	}
 
-    def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
+	def doWithApplicationContext = { applicationContext ->
+	}
 
-    def onShutdown = { event ->
-        // TODO Implement code that is executed when the application shuts down (optional)
-    }
+	def onChange = { event ->
+	}
+
+	def onConfigChange = { event ->
+	}
+
+	def onShutdown = { event ->
+	}
+	//*/
 }
